896b7652861fa753df8eadbac4bd0dd2af12338d
zhengxiaowen_g <zh951296508_g@qq.com>
2025-12-13 09:58:43 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,5 @@
 import { useAppStore } from "@/store";
 console.log("kkkkkkkkkkkkkkk", useAppStore().isCollapse);
 let a = import.meta.env.VITE_APP_TITLE;
-console.log("aaaaaaaaaaaaaaaaaa", a);
 </script>
 <style scoped></style>
